026561ca5320a095e55b91810a5b696f180231ff
Heitor Neto <heitorh3@gmail.com>
2016-04-18 08:28:47 -0300

    Configuração do gitIgnore

---
+++ target/classes/resources/javascripts/app.js
@@ -0,0 +1,11 @@
+$(function() {
+	$('.js-toggle').bind('click', function(event) {
+		$('.js-sidebar, .js-content').toggleClass('is-toggled');
+		event.preventDefault();
+	});	
+	
+	$('.js-selected').bind('click', function(event) {
+		$('.js-selected').toggleClass('is-selected');		
+		event.preventDefault();
+	});	
+});
