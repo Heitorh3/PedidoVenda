0f0c134b4702f93a6178672e95135d6b0782ae12
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2016-11-15 11:10:31 -0200

    Adicionando as bibliotecas do projeto

--- src/main/java/br/com/model/util/cdi/EntityManagerProducer.java
+++ src/main/java/br/com/model/util/cdi/EntityManagerProducer.java
@@ -14,7 +14,7 @@ public class EntityManagerProducer {
 	private EntityManagerFactory factory;
 	
 	public EntityManagerProducer() {
-		this.factory = Persistence.createEntityManagerFactory("sigepePU");
+		this.factory = Persistence.createEntityManagerFactory("PedidoVendaPU");
 	}
 	
 	@Produces
